526ba64ae009e47b07eb83072413650644296b95
nailboy <omomo010@gmail.com>
2022-02-17 16:05:07 +0900

    class commit

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "ZoMD",
-    age: 24,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+} // TS 측면에선 nterface 더 안전하나 다른 프레임워크를 사용할 땐 class를 사용해야함
+const zomd = new Human("ZoMD", 25, "male");
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
-console.log(sayHi(person));
+console.log(sayHi(zomd));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-  }
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string; //private를 하면 클래스 외에서 사용되지않게 보호함
+    constructor(name: string, age: number, gender: string) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+    }
+  } // TS 측면에선 nterface 더 안전하나 다른 프레임워크를 사용할 땐 class를 사용해야함
   
-  const person = {
-    name: "ZoMD",
-    age: 24,
-    gender: "male"
-  };
+  const zomd = new Human("ZoMD", 25, "male");
   
   const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${
@@ -16,6 +17,6 @@ interface Human {
     }!`;
   };
 
-  console.log(sayHi(person));
+  console.log(sayHi(zomd));
   
   export {};
