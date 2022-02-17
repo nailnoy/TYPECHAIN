908d262df8f5f657728daa4f5e191d355e65f7de
nailboy <omomo010@gmail.com>
2022-02-17 18:55:01 +0900

    Creating a Block_one

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const CryptoJS = require("crypto-js");
 class Block {
     constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
@@ -9,8 +10,13 @@ class Block {
         this.timestamp = timestamp;
     }
 }
+//Hash키를 생성하기 위한 메소드 (static으로 선언함으로서 class를 선언하지 않아도 사용 가능)
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 const genesisBlock = new Block(0, "20220217", "", "ZoMD", 123456);
 let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 //blockchain.push("stuff"); class Block의 구조를 지키지않았기때문에 실행 X
 console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+     //Hash키를 생성하기 위한 메소드 (static으로 선언함으로서 class를 선언하지 않아도 사용 가능)
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
       index: number,
       hash: string,
@@ -21,7 +31,14 @@ class Block {
   
   const genesisBlock: Block = new Block(0, "20220217", "", "ZoMD", 123456);
   
-  let blockchain: [Block] = [genesisBlock];
+  let blockchain: Block[] = [genesisBlock];
+
+  const getBlockchain = (): Block[] => blockchain;
+
+  const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+  const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
 
   //blockchain.push("stuff"); class Block의 구조를 지키지않았기때문에 실행 X
   console.log(blockchain);
