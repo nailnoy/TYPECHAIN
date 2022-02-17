7fec088ce58451a223b2eabf182cee345fbd4dda
nailboy <omomo010@gmail.com>
2022-02-17 19:31:53 +0900

    Validating Block Structure

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,12 @@ class Block {
 }
 //Hash키를 생성하기 위한 메소드 (static으로 선언함으로서 class를 선언하지 않아도 사용 가능)
 Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+//블록의 구조를 인증하기 위한 메소드
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 const genesisBlock = new Block(0, "20220217", "", "ZoMD", 123456);
 let blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
@@ -20,15 +26,27 @@ const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 //새로운 timestamp를 받아오는 함수
 const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
+//새로운 블록을 만드는 함수
 const createNewBlock = (data) => {
-    const previosBlock = getLatestBlock();
-    const newIndex = previosBlock.index + 1;
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
     const newTimestamp = getNewTimeStamp();
-    const newHash = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
-    const newBlock = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp);
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     return newBlock;
 };
 //blockchain.push("stuff"); class Block의 구조를 지키지않았기때문에 실행 X
-console.log(createNewBlock("Hi"));
-console.log(createNewBlock("umm"));
+// 블록이 유효한지 체크하는 함수
+const isBlockValid = (candidateBlock, previousBlock) => {
+    //블록이 유효한지 체크
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } //이전 블록의 인덱스 + 1과 현재 블록의 인덱스가 같은지 체크
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } //이전 블록의 해쉬와 현재 블록의 이전해쉬가 같은지 체크
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,26 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-
      //Hash키를 생성하기 위한 메소드 (static으로 선언함으로서 class를 선언하지 않아도 사용 가능)
     static calculateBlockHash = (
         index:number, 
         previousHash:string, 
         timestamp:number, 
         data:string) : string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+     //블록의 구조를 인증하기 위한 메소드
+    static validateStructure = (aBlock : Block) : boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" && 
+    typeof aBlock.timestamp === "number" && 
+    typeof aBlock.data === "string";
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
     constructor(
       index: number,
@@ -42,20 +50,21 @@ class Block {
   //새로운 timestamp를 받아오는 함수
   const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
+  //새로운 블록을 만드는 함수
   const createNewBlock = (data: string): Block => {
-    const previosBlock: Block = getLatestBlock();
-    const newIndex: number = previosBlock.index + 1;
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
     const newTimestamp: number = getNewTimeStamp();
     const newHash: string = Block.calculateBlockHash(
       newIndex,
-      previosBlock.hash,
+      previousBlock.hash,
       newTimestamp,
       data
     );
     const newBlock: Block = new Block(
       newIndex,
       newHash,
-      previosBlock.hash,
+      previousBlock.hash,
       data,
       newTimestamp
     );
@@ -65,7 +74,18 @@ class Block {
 
   //blockchain.push("stuff"); class Block의 구조를 지키지않았기때문에 실행 X
 
-  console.log(createNewBlock("Hi"));
-  console.log(createNewBlock("umm"));
+   // 블록이 유효한지 체크하는 함수
+  const isBlockValid = (candidateBlock : Block, previousBlock : Block) : boolean => {
+    //블록이 유효한지 체크
+    if(!Block.validateStructure(candidateBlock)){
+        return false;
+    } //이전 블록의 인덱스 + 1과 현재 블록의 인덱스가 같은지 체크
+    else if(previousBlock.index + 1 !== candidateBlock.index){
+        return false;
+    } //이전 블록의 해쉬와 현재 블록의 이전해쉬가 같은지 체크
+    else if(previousBlock.hash !== candidateBlock.previousHash){
+        return false;
+    } 
+};
   
   export {};
